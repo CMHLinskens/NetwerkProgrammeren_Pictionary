8f6e4f2d7dc3adafc5671ed8b1e45df321b6f4a4
Bart van Moorsel <bartvanmoorsel7@gmail.com>
2020-06-07 18:02:38 +0200

    Added displayabable guessWord on guessCanvas

--- Pictionary/src/data/DataSingleton.java
+++ Pictionary/src/data/DataSingleton.java
@@ -12,7 +12,7 @@ public final class DataSingleton extends Observable {
     private static DataSingleton INSTANCE;
     private Client client;
     private DrawData drawData;
-    private boolean isDrawing;
+    private SimpleBooleanProperty isDrawing = new SimpleBooleanProperty(false);
     private String message = "";
     private String sendMessage = "";
 
@@ -52,11 +52,11 @@ public final class DataSingleton extends Observable {
         setChanged();
         notifyObservers();
     }
-    public boolean isDrawing() {
+    public SimpleBooleanProperty isDrawing() {
         return isDrawing;
     }
     public void setDrawing(boolean drawing) {
-        isDrawing = drawing;
+        isDrawing.set(drawing);
     }
     public String getMessage() {
         return message;
